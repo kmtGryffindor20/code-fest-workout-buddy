8fcdddec6035914b76b3ea5b017c7a64a7431677
kmtGryffindor20 <tripathikmt20@gmail.com>
2023-10-05 18:30:00 +0530

    moved images from public to assets

--- src/components/WorkoutArea.jsx
+++ src/components/WorkoutArea.jsx
@@ -1,6 +1,7 @@
 import Workout from "./Workout"
 import CreateWorkout from "./CreateWorkout"
 import { useState, useEffect } from "react"
+import plus from "../assets/images/plus.png"
 export default function WorkoutArea(props){
 
     const [data, setData] = useState([])
@@ -53,7 +54,7 @@ export default function WorkoutArea(props){
         <div id="workoutArea" className=" text-white py-10 w-full h-auto px-8 sm:px-32">
             <div className="flex items-center mb-6">
                 <h5 className="indent-2 font-bold text-lg sm:text-xl md:text-3xl">Your Workouts</h5>
-                <a onClick={handleModal} className="ml-0 sm:ml-5 items-center hover:animate-pulse cursor-pointer"><img className="w-10" src="/images/plus.png" alt="" /></a> 
+                <a onClick={handleModal} className="ml-0 sm:ml-5 items-center hover:animate-pulse cursor-pointer"><img className="w-10" src={plus} alt="" /></a> 
             </div>
             {isOpen && <CreateWorkout setOpen={setOpen} baseURI={props.baseURI} token={props.token} setReload={setReload} />}
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 lg:gap-10">
